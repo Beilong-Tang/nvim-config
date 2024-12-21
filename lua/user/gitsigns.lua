189833046743bf512bc5fb66aa5a7b34eb68ba28
Beilong-Tang <bt132@duke.edu>
2024-12-21 17:38:59 +0800

    update color

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -2,15 +2,36 @@ local status_ok, gitsigns = pcall(require, "gitsigns")
 if not status_ok then
   return
 end
+-- Define highlight groups for GitSigns
+-- Define custom colors for GitSigns highlights
+vim.api.nvim_set_hl(0, 'GitSignsAdd', { fg = '#00ff00', bg = '#002200' })
+vim.api.nvim_set_hl(0, 'GitSignsAddLn', { fg = '#00ff00', bg = '#002200' })
+vim.api.nvim_set_hl(0, 'GitSignsAddNr', { fg = '#00ff00', bg = '#002200' })
+
+vim.api.nvim_set_hl(0, 'GitSignsChange', { fg = '#ffff00', bg = '#222200' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeLn', { fg = '#ffff00', bg = '#222200' })
+vim.api.nvim_set_hl(0, 'GitSignsChangeNr', { fg = '#ffff00', bg = '#222200' })
+
+vim.api.nvim_set_hl(0, 'GitSignsChangedelete', { fg = '#ff8800', bg = '#222200' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteLn', { fg = '#ff8800', bg = '#222200' })
+vim.api.nvim_set_hl(0, 'GitSignsChangedeleteNr', { fg = '#ff8800', bg = '#222200' })
+
+vim.api.nvim_set_hl(0, 'GitSignsDelete', { fg = '#ff0000', bg = '#220000' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteLn', { fg = '#ff0000', bg = '#220000' })
+vim.api.nvim_set_hl(0, 'GitSignsDeleteNr', { fg = '#ff0000', bg = '#220000' })
+
+vim.api.nvim_set_hl(0, 'GitSignsTopdelete', { fg = '#ff0000', bg = '#220000' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteLn', { fg = '#ff0000', bg = '#220000' })
+vim.api.nvim_set_hl(0, 'GitSignsTopdeleteNr', { fg = '#ff0000', bg = '#220000' })
 
 gitsigns.setup {
-  signs = {
-    add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-    change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-    delete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    topdelete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-    changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-  },
+  -- signs = {
+  --   add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+  --   change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+  --   delete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+  --   topdelete = { hl = "GitSignsDelete", text = "", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+  --   changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+  -- },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -28,7 +28,7 @@ local options = {
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = false,                  -- set relative numbered lines
-  numberwidth = 4,                         -- set number column width to 2 {default 4}
+    numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = true,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
@@ -47,3 +47,5 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+vim.opt.fileformat = "unix"
