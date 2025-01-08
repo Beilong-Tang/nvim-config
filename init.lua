d632be8e993c99e5bb010263ab25fad2f189869f
Beilong <bt132@duke.edu>
2025-01-08 15:14:31 +0800

    u

--- init.lua
+++ init.lua
@@ -18,3 +18,11 @@ require "user.black"
 -- If on a server, can enable this 
 -- require "user.gitsigns" -- # This is disabled due to performance issues on local machine. It should be fine on server.
 -- require "user.server" -- # Enable copy on a server
+
+
+require("catppuccin").setup({
+    no_italic = true, -- Force no italic
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin-latte"
